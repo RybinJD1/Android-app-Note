0a2cde6836240c559dc138a648870a96c1a1a6c3
RybinJD1 <alexfisher2013@gmail.com>
2022-09-25 20:14:30 +0200

    add deleteNote use case

---
+++ app/src/main/java/com/mycoding/noteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,13 @@
+package com.mycoding.noteapp.feature_note.domain.use_case
+
+import com.mycoding.noteapp.feature_note.domain.model.Note
+import com.mycoding.noteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
