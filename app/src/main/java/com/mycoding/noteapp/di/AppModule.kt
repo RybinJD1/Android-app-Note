4331099b575592da51b6bc0df304d080782f03b5
RybinJD1 <alexfisher2013@gmail.com>
2022-09-27 17:40:57 +0200

    add AddEditNoteViewModel

--- app/src/main/java/com/mycoding/noteapp/di/AppModule.kt
+++ app/src/main/java/com/mycoding/noteapp/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.mycoding.noteapp.feature_note.data.data_source.NoteDatabase
 import com.mycoding.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.mycoding.noteapp.feature_note.domain.repository.NoteRepository
-import com.mycoding.noteapp.feature_note.domain.use_case.AddNote
-import com.mycoding.noteapp.feature_note.domain.use_case.DeleteNote
-import com.mycoding.noteapp.feature_note.domain.use_case.GetNotes
-import com.mycoding.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.mycoding.noteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,7 +38,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
